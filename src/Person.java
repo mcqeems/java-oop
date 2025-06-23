df087350b8c84becb19e47160da44b23ccb20148
mcqeems <mcqeemsofficial@gmail.com>
2025-06-23 21:04:18 +0700

    This keyword

--- src/Person.java
+++ src/Person.java
@@ -1,10 +1,25 @@
 public class Person {
+	// Attrbutes
 	String name;
 	String address;
 	final String country = "Indonesia";
 
-	String sayHello(String paramName) {
-		String say = "Hello " + paramName + ", my name is " + name;
+	Person(String name, String address) {
+		this.name = name;
+		this.address = address;
+	}
+
+	Person(String name) {
+		this(name, null);
+	}
+
+	Person() {
+		this(null);
+
+	}
+
+	String sayHello(String name) {
+		String say = "Hello " + this.name + ", my name is " + name;
 		return say;
 	}
 }
--- src/PersonApp.java
+++ src/PersonApp.java
@@ -2,30 +2,20 @@ public class PersonApp {
 
     public static void main(String[] args) {
 
-        // Setting up object from Person Class by fullfilling all the requirements.
-        var person1 = new Person();
-        String person1Name = person1.name = "Mustaqim";
-        String person1Address = person1.address = "Karawang";
-        String person1Country = person1.country;
-        String person1Say = person1.sayHello("Budi");
+        var person1 = new Person("Mustaqim", "Karawang");
+        var person1Say = person1.sayHello("Eko");
 
-        // Setting up object from Person Class without fullfilling all the requirements.
-        var person2 = new Person();
-        String person2Name = person2.name = "Budi";
-        String person2Address = person2.address; // Person Address is not fullfilled which lead the variable null (//
-                                                 // Default stat of String data type)
-        String person2Country = person2.country;
-        String person2Say = person2.sayHello("Mustaqim");
+        var person2 = new Person("Eko", "Jakarta");
+        var person2Say = person2.sayHello("Mustaqim");
 
-        // Print all the objects
-        System.out.println(person1Name);
-        System.out.println(person1Address);
-        System.out.println(person1Country);
+        System.out.println(person1.name);
+        System.out.println(person1.address);
+        System.out.println(person1.country);
         System.out.println(person1Say);
 
-        System.out.println(person2Name);
-        System.out.println(person2Address); // Expected output Null
-        System.out.println(person2Country);
+        System.out.println(person2.name);
+        System.out.println(person2.address);
+        System.out.println(person2.country);
         System.out.println(person2Say);
     }
 }
