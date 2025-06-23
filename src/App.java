0ebe834dff23e6b3978a01e46269dca7af0a0814
mcqeems <mcqeemsofficial@gmail.com>
2025-06-23 19:12:21 +0700

    Initial commit

--- src/App.java
+++ src/App.java
@@ -1,6 +1,15 @@
 public class App {
 
     public static void main(String[] args) {
+        var person1 = new Person();
+        String person1Name = person1.name = "Mustaqim";
+        String person1Address = person1.address = "Karawang";
+        String person1Country = person1.country;
+        String person1Say = person1.sayHello("Budi");
 
+        System.out.println(person1Name);
+        System.out.println(person1Address);
+        System.out.println(person1Country);
+        System.out.println(person1Say);
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -2,4 +2,9 @@ public class Person {
 	String name;
 	String address;
 	final String country = "Indonesia";
+
+	String sayHello(String paramName) {
+		String say = "Hello " + paramName + ", my name is " + name;
+		return say;
+	}
 }
